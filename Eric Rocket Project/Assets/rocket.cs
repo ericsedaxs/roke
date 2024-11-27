0849d70bbc87985a0f00948b6de77dfc23dd68dd
Andrew Park <ajpark5@uci.edu>
2024-11-26 17:55:25 -0800

    Revert "checkpoint"
    This reverts commit e3efcf882b665e96f93cc87079b7100b859eb46f.

--- Eric Rocket Project/Assets/rocket.cs
+++ Eric Rocket Project/Assets/rocket.cs
@@ -24,7 +24,6 @@ public class rocket : Agent
         rb.angularVelocity = Vector3.zero;
         transform.localPosition = new Vector3(0, 500f, 0);
         previousPosition = transform.localPosition;
-        lastY = 1000f;
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -149,25 +148,19 @@ public class rocket : Agent
         previousPosition = transform.localPosition;
 
         if (transform.localPosition.y < (platform.transform.localPosition.y - 10)) {
-            Debug.Log("Failed because of going below the platform");
             SetReward(-1.0f);
             platform.GetComponent<MeshRenderer>().material = failMaterial;
-            lastY = 1000f;
             EndEpisode();
         }
 
         // ignore in heuristic mode
-        // bool isHeuristic = Academy.Instance.IsCommunicatorOn;
-        if (transform.localPosition.y > (lastY)) {
-            Debug.Log("Failed because of going up");
+        bool isHeuristic = Academy.Instance.IsCommunicatorOn;
+        if (transform.localPosition.y > (lastY) && !isHeuristic) {
             SetReward(-1.0f);
             platform.GetComponent<MeshRenderer>().material = failMaterial;
-            lastY = 1000f;
             EndEpisode();
         }
 
         lastY = transform.localPosition.y;
     }
 }
-
-// https://github.com/gzrjzcx/ML-agents/blob/master/docs/Training-Imitation-Learning.md
