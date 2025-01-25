2338fd558bde441e7f684240a2d189cf6b5b0e6a
Andrew Park <ajpark5@uci.edu>
2025-01-25 14:29:18 -0800

    checkpoint

--- Eric Rocket Project/Assets/rocket.cs
+++ Eric Rocket Project/Assets/rocket.cs
@@ -34,47 +34,55 @@ public class rocket : Agent
     public TMP_InputField massInput;
 
     float lastY = 1000f;
+    float targetSpeed = -15.0f;
 
     public override void OnEpisodeBegin()
     {
-        powerInput.text = power.ToString();
-        massInput.text = rb.mass.ToString();
+        if (powerInput != null && massInput != null) {
+            powerInput.text = power.ToString();
+            massInput.text = rb.mass.ToString();
+        }
+        
 
         lastY = 1000f;
         rb.linearVelocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
         transform.localPosition = new Vector3(0, 500f, 0);
+        transform.localRotation = Quaternion.identity;
         previousPosition = transform.localPosition;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(rb.linearVelocity);
+        sensor.AddObservation(transform.localPosition); // 3
+        sensor.AddObservation(rb.linearVelocity); // 3
 
         // rotation
-        sensor.AddObservation(transform.rotation);
+        sensor.AddObservation(transform.rotation); // 4
 
         // drag
-        sensor.AddObservation(rb.linearDamping);
+        sensor.AddObservation(rb.linearDamping); // 1
 
         // angular drag
-        sensor.AddObservation(rb.angularDamping);
+        sensor.AddObservation(rb.angularDamping); // 1
 
         // mass
-        sensor.AddObservation(rb.mass);
+        sensor.AddObservation(rb.mass); // 1
 
         // gravity
-        sensor.AddObservation(Physics.gravity);
+        sensor.AddObservation(Physics.gravity); // 3
 
         // goal location
-        sensor.AddObservation(platform.transform.localPosition);
+        sensor.AddObservation(platform.transform.localPosition); // 3
+
+        // target speed
+        sensor.AddObservation(targetSpeed); // 1
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
         var discreteActions = actions.DiscreteActions;
-        // Debug.Log(discreteActions[0]);
+        Debug.Log($"Discrete Actions: {discreteActions[0]} {discreteActions[1]} {discreteActions[2]} {discreteActions[3]} {discreteActions[4]}");
         if (discreteActions[0] == 1) {
             thrusterOn = true;
             mainThrusterParticles.SetActive(true);
@@ -87,41 +95,41 @@ public class rocket : Agent
             rb.AddForce(transform.up * power);
         }
 
-        // if (discreteActions[1] == 1) {
-        //     Debug.Log("North Thruster On");
-        //     // rotate the rocket north
-        //     transform.Rotate(Vector3.right * 0.1f);
-        //     northThrusterParticles.SetActive(true);
-        // } else {
-        //     northThrusterParticles.SetActive(false);
-        // }
+        if (discreteActions[1] == 1) {
+            Debug.Log("North Thruster On");
+            // rotate the rocket north
+            transform.Rotate(Vector3.right * 0.1f);
+            northThrusterParticles.SetActive(true);
+        } else {
+            northThrusterParticles.SetActive(false);
+        }
 
-        // if (discreteActions[2] == 1) {
-        //     Debug.Log("East Thruster On");
-        //     // rotate the rocket east
-        //     transform.Rotate(Vector3.forward * 0.1f);
-        //     eastThrusterParticles.SetActive(true);
-        // } else {
-        //     eastThrusterParticles.SetActive(false);
-        // }
+        if (discreteActions[2] == 1) {
+            Debug.Log("East Thruster On");
+            // rotate the rocket east
+            transform.Rotate(Vector3.forward * 0.1f);
+            eastThrusterParticles.SetActive(true);
+        } else {
+            eastThrusterParticles.SetActive(false);
+        }
 
-        // if (discreteActions[3] == 1) {
-        //     Debug.Log("South Thruster On");
-        //     // rotate the rocket south
-        //     transform.Rotate(Vector3.right * -0.1f);
-        //     southThrusterParticles.SetActive(true);
-        // } else {
-        //     southThrusterParticles.SetActive(false);
-        // }
+        if (discreteActions[3] == 1) {
+            Debug.Log("South Thruster On");
+            // rotate the rocket south
+            transform.Rotate(Vector3.right * -0.1f);
+            southThrusterParticles.SetActive(true);
+        } else {
+            southThrusterParticles.SetActive(false);
+        }
 
-        // if (discreteActions[4] == 1) {
-        //     Debug.Log("West Thruster On");
-        //     // rotate the rocket west
-        //     transform.Rotate(Vector3.forward * -0.1f);
-        //     westThrusterParticles.SetActive(true);
-        // } else {
-        //     westThrusterParticles.SetActive(false);
-        // }
+        if (discreteActions[4] == 1) {
+            Debug.Log("West Thruster On");
+            // rotate the rocket west
+            transform.Rotate(Vector3.forward * -0.1f);
+            westThrusterParticles.SetActive(true);
+        } else {
+            westThrusterParticles.SetActive(false);
+        }
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -165,13 +173,13 @@ public class rocket : Agent
         if (collision.gameObject.CompareTag("Goal")) {
             // check vertical speed
 
-            if (currentVelocity.y >= -15) {
-                SetReward(20.0f);
+            if (currentVelocity.y >= targetSpeed) {
+                SetReward(10.0f);
                 Debug.Log("Landed with a velocity of: " + currentVelocity.y + " m/s");
                 platform.GetComponent<MeshRenderer>().material = successMaterial;
                 EndEpisode();
             } else {
-                SetReward(-20.0f);
+                SetReward(-10.0f);
                 Debug.Log("Crashed with a velocity of: " + currentVelocity.y + " m/s");
                 platform.GetComponent<MeshRenderer>().material = failMaterial;
                 EndEpisode();
@@ -181,8 +189,11 @@ public class rocket : Agent
 
     // Update is called once per frame
     void FixedUpdate() {
-        power = float.Parse(powerInput.text);
-        rb.mass = float.Parse(massInput.text);
+
+        if (powerInput != null && massInput != null) {
+            power = float.Parse(powerInput.text);
+            rb.mass = float.Parse(massInput.text);
+        }
 
         currentVelocity = (transform.localPosition - previousPosition) / Time.deltaTime;
         // Debug.Log($"{transform.localPosition} - {previousPosition} / {Time.deltaTime} = {currentVelocity}");
@@ -190,7 +201,7 @@ public class rocket : Agent
 
         if (transform.localPosition.y < (platform.transform.localPosition.y - 10)) {
             Debug.Log("Failed because of going below the platform");
-            SetReward(-10.0f);
+            SetReward(-15.0f);
             platform.GetComponent<MeshRenderer>().material = failMaterial;
             lastY = 1000f;
             EndEpisode();
@@ -209,7 +220,7 @@ public class rocket : Agent
 
         if (transform.localPosition.y > 550f) {
             Debug.Log("Failed because of going too high");
-            AddReward(-10.0f);
+            AddReward(-20.0f);
             platform.GetComponent<MeshRenderer>().material = failMaterial;
             lastY = 1000f;
             EndEpisode();
